51b7912ee094c903a41a033e2b2eb6458d3df7a6
galcyurio <galcyurio@gmail.com>
2023-10-11 21:22:15 +0900

    MessageCard를 별도 파일로 분리한다

--- app/src/main/java/net/slipp/MainActivity.kt
+++ app/src/main/java/net/slipp/MainActivity.kt
@@ -3,30 +3,10 @@ package net.slipp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import net.slipp.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,58 +22,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun MessageCard(
-    author: String,
-    body: String,
-) {
-    Row(
-        modifier = Modifier
-            .padding(all = 8.dp)
-            .fillMaxWidth()
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.img),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .size(64.dp)
-                .clip(CircleShape)
-                .border(2.dp, Color.Black, CircleShape)
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        Column {
-            Text(
-                text = author,
-                color = Color.DarkGray,
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            BodyText(text = body)
-        }
-    }
-}
-
-@Composable
-private fun BodyText(text: String) {
-    Surface(
-        modifier = Modifier
-            .border(1.dp, Color.LightGray, RoundedCornerShape(100.dp))
-            .padding(all = 4.dp)
-    ) {
-        Text(text = text)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun MessageCardPreview() {
-    ComposeTutorialTheme {
-        MessageCard(
-            author = "Android",
-            body = "Jetpack Compose",
-        )
     }
 }
