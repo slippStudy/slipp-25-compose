a36e25e9d8d979c3cd6669664b2af2892abb009f
ChoiUS <chldn323@gmail.com>
2023-10-11 21:52:47 +0900

    - Android 앱에 이미지 추가

--- week1_1/app/src/main/java/com/example/week1_1/MainActivity.kt
+++ week1_1/app/src/main/java/com/example/week1_1/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.week1_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +16,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,20 +35,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
+                    GreetingImage(
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_text)
+                    )
                 }
             }
         }
     }
 }
 
-@Preview(showBackground = true, heightDp = 1000)
-@Composable
-fun BirthdayCardPreview() {
-    Week1_1Theme {
-        GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
-    }
-}
+//@Preview(showBackground = true, heightDp = 1000)
+//@Composable
+//fun BirthdayCardPreview() {
+//    Week1_1Theme {
+//        GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
+//    }
+//}
 
 @Composable
 fun GreetingText(
@@ -64,8 +72,39 @@ fun GreetingText(
         Text(
             text = from,
             fontSize = 36.sp,
-            modifier = Modifier.padding(16.dp)
-                .align(alignment = Alignment.End)
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            message = message, from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    Week1_1Theme {
+        GreetingImage(
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.signature_text)
         )
     }
 }
