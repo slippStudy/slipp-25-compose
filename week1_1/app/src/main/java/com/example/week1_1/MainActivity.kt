c15e0abb7ae1ca9b196174e0706dee3afce02c28
ChoiUS <chldn323@gmail.com>
2023-10-11 21:18:29 +0900

    - 텍스트 컴포저블로 간단한 앱 빌드 코드랩 수행

---
+++ week1_1/app/src/main/java/com/example/week1_1/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.week1_1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.week1_1.ui.theme.Week1_1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Week1_1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, heightDp = 1000)
+@Composable
+fun BirthdayCardPreview() {
+    Week1_1Theme {
+        GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
+    }
+}
+
+@Composable
+fun GreetingText(
+    message: String,
+    from: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp),
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center,
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier.padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
+}
