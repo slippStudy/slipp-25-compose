dbbe28a03ce064e81f4dcb8d85eb20b920a74407
galcyurio <galcyurio@gmail.com>
2023-10-11 21:19:16 +0900

    BodyText에 테두리를 추가한다

--- app/src/main/java/net/slipp/MainActivity.kt
+++ app/src/main/java/net/slipp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,12 +15,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -59,19 +62,34 @@ fun MessageCard(
             modifier = Modifier
                 .size(64.dp)
                 .clip(CircleShape)
+                .border(2.dp, Color.Black, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = author)
+            Text(
+                text = author,
+                color = Color.DarkGray,
+            )
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = body)
+            BodyText(text = body)
         }
+    }
+}
+
+@Composable
+private fun BodyText(text: String) {
+    Surface(
+        modifier = Modifier
+            .border(1.dp, Color.LightGray, RoundedCornerShape(100.dp))
+            .padding(all = 4.dp)
+    ) {
+        Text(text = text)
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun MessageCardPreview() {
+private fun MessageCardPreview() {
     ComposeTutorialTheme {
         MessageCard(
             author = "Android",
